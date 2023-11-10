8b6b1cf2a3201343e7de4d1768b113384cc455f8
John Dexter Lanot <21-09401@g.batstate-u.edu.ph>
2023-11-09 19:46:38 -0800

    test

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,24 +1,24 @@
-import './App.css';
-import {Routes, Route} from 'react-router-dom'
+import './App.css'; 
+import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Navbar from './components/Navbar';
 
-
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/' element={<Login/>} />
-        <Route path='/' element={<Signup/>} />
-        <Route path='/' element={<Dashboard/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </div>
   );
 }
+
 
 export default App;
--- frontend/client/src/components/Navbar.js
+++ frontend/client/src/components/Navbar.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../index.css';
+import logo from '../assets/client-logo.png';
 
-function Navbar () {
-    return (
-        <nav>
-            
-        </nav>
-    )
+function Navbar() {
+  return (
+    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+      <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
+        <Link to="/" className="flex items-center">
+          <img src={logo} className="h-8 mr-3" alt="Logo" />
+        </Link>
+        <ul className="flex space-x-4">
+          <li>
+            <Link to="/login" className="text-gray-900 hover:text-blue-700">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link to="/signup" className="text-gray-900 hover:text-blue-700">
+              Signup
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
